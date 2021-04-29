80acd557c4ccd861043efcb6f538e11ea273b272
Willian Prado <willian_prado@hotmail.com>
2021-04-29 13:49:38 -0300

    Completa o desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,36 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(myArray, name) {
+
+  if (myArray == false){
+    return "Vazio!";
+  }
+  
+  let newArray = [];
+
+  for (let index in myArray){
+    objeto = {
+      tech: myArray[index],
+      name: name
+    }
+    newArray.push(objeto);
+  }
+
+  newArray.sort(function(a, b){
+    if (a.tech < b.tech){
+        return -1;
+    } else if (a.tech > b.tech){
+        return 1;
+    } else if (a.tech === b.tech){
+        return 0
+    }
+  })
+
+  return newArray;
+
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+console.log(techList([], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
